89482d263722a16342fab4150a498091c9526d98
Denis Brester <49287397+DenKick@users.noreply.github.com>
2020-10-05 17:43:19 +0300

    Add files via upload

---
+++ precache-manifest.471c6e7892eccf20a8f3e7dd84df8754.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39652562c3fdad9825658a95ceb03e2d",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "e8156e4e76a18a369030",
+    "url": "/test-react/static/css/main.5324de0c.chunk.css"
+  },
+  {
+    "revision": "8c0d802e96583827cf00",
+    "url": "/test-react/static/js/2.fd75d1d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/test-react/static/js/2.fd75d1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8156e4e76a18a369030",
+    "url": "/test-react/static/js/main.0f97ac67.chunk.js"
+  },
+  {
+    "revision": "749a69bc2f1e6da32b80",
+    "url": "/test-react/static/js/runtime-main.0b88a9d2.js"
+  },
+  {
+    "revision": "54d8e0a9872b2cccba901187966ed13e",
+    "url": "/test-react/static/media/icomoon.54d8e0a9.woff"
+  },
+  {
+    "revision": "bdb7c3f88e008627c771ce39eafe92b7",
+    "url": "/test-react/static/media/icomoon.bdb7c3f8.ttf"
+  },
+  {
+    "revision": "be4257c5f8453b1641dde1afdce37941",
+    "url": "/test-react/static/media/icomoon.be4257c5.eot"
+  },
+  {
+    "revision": "f8fe2841a277d144c571f6e4739968c4",
+    "url": "/test-react/static/media/icomoon.f8fe2841.svg"
+  }
+]);
